234af63ab52c77eb8a56ca9c772c83a73c1b5825
fbenfraj <farouk.ben-fraj@outlook.com>
2022-03-31 20:58:49 +0100

    Initialize web3

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-import web3 from "./src/web3";
+const express = require("express");
+const Web3 = require("web3");
 
 require("dotenv").config();
-const express = require("express");
 
+// WEB3 CONFIG
+const provider = new Web3.providers.HttpProvider(process.env.RPC_URL);
+
+web3 = new Web3(provider);
+
+// SERVER CONFIG
 const port = process.env.PORT || 5000;
 const app = express();
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}! :)`);
+  console.log(`Listening on port ${port} :)`);
 });
