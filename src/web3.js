2b9b3bebc96405dfbba0c8ab18b820a84a56d2b4
fbenfraj <farouk.ben-fraj@outlook.com>
2022-04-01 15:28:48 +0100

    Update web3 module to use HDWalletProvider

--- src/web3.js
+++ src/web3.js
@@ -1,5 +1,12 @@
 const Web3 = require("web3");
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 
-web3 =  new Web3(process.env.RPC_URL)
+require("dotenv").config();
+
+const provider = new Web3(
+  new HDWalletProvider(process.env.PRIVATE_KEY, process.env.RPC_URL)
+);
+
+const web3 = new Web3(provider);
 
 module.exports = web3;
