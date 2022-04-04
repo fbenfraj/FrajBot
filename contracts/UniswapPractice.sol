e440b117ef5643b3d62913a755c04238a3455a51
fbenfraj <farouk.ben-fraj@outlook.com>
2022-04-04 15:41:51 +0100

    Uniswap SDK and contract to swap tokens, ethers dependency

---
+++ contracts/UniswapPractice.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+interface IUniswap {
+    function swapExactTokensForETH(
+        uint256 amountIn,
+        uint256 amountOutMin,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) external returns (uint256[] memory amounts);
+
+    function WETH() external pure returns (address);
+}
+
+interface IERC20 {
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) external returns (bool);
+
+    function approve(address spender, uint256 amount) external returns (bool);
+}
+
+contract UniswapPractice {
+    IUniswap uniswap;
+
+    constructor(address _uniswap) {
+        uniswap = IUniswap(_uniswap);
+    }
+
+    function swapTokensForETH(
+        address token,
+        uint256 amountIn,
+        uint256 amountOutMin,
+        uint256 deadline
+    ) external {
+        IERC20(token).transferFrom(msg.sender, address(this), amountIn);
+        address[] memory path = new address[](2);
+        path[0] = token;
+        path[1] = uniswap.WETH();
+        IERC20(token).approve(address(uniswap), amountIn);
+        uniswap.swapExactTokensForETH(
+            amountIn,
+            amountOutMin,
+            path,
+            msg.sender,
+            deadline
+        );
+    }
+}
